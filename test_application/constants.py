3410177f12d41c9a9e4dff4da36f1e0625f6dde2
anoopunni1992 <83015071+anoopunni1992@users.noreply.github.com>
2021-10-11 13:22:48 +0530

    Add files via upload

--- test_application/constants.py
+++ test_application/constants.py
@@ -1,1 +1,7 @@
-test
+#----Code Developed By : Anoop U
+#----Date: 07-oct-2021
+#----purpose: LinkFire Interview Task 
+#----Code Logic: Define constants used in the application
+
+TOPIC_1='test'
+TOPIC_2='old_topic'
